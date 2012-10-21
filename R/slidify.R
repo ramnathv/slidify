6efdc1a690c129bb66e61becb5ee3d7ea9769b63
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2012-10-21 14:41:20 -0400

    modify order in which functions are run

--- R/slidify.R
+++ R/slidify.R
@@ -1,19 +1,30 @@
 slidify <- function(inputFile, outputFile, knit_deck = T){
+	# knit input file if knit_deck is TRUE
   if (knit_deck){
     inputFile = inputFile %|% knit
   }
+  
+  # split deck into metadata and slides, copying libraries if indicated
   deck = inputFile %|% to_deck 
   if (deck$copy_libraries){
   	with(deck, copy_libraries(framework, highlighter, widgets))
   }
-  deck = deck %|% add_urls %|% add_stylesheets
+  
+  # split slides, parse into elements and add slide numbers and ids
   deck$slides = deck$slides %|% split_slides %|% parse_slides 
+  deck$slides = deck$slides %|% add_slide_numbers %|% add_missing_id
+  
+  # add layouts, urls and stylesheets from frameworks, widgets and assets
+  deck = deck %|% add_urls %|% add_stylesheets
   layouts = get_layouts(deck$url$layouts)
   layouts = modifyList(layouts, list(javascripts = get_javascripts(deck)))
+  
+ 
+  
   if (missing(outputFile)){
     outputFile = gsub("*.[R]?md$", '.html', inputFile)
   }
-  deck$slides = deck$slides %|% add_slide_numbers %|% add_missing_id
+  
   cat(render_deck(deck, layouts), file = outputFile)
   return(outputFile)
 }
