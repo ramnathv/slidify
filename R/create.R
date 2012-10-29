94f61a947b9c7740880532916f1b48ab8c74231e
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2012-10-29 16:21:00 -0400

    automatically detect if git is installed

--- R/create.R
+++ R/create.R
@@ -1,24 +1,25 @@
 #' Initialize a slide deck
 #' 
-create_deck <- function(deckdir, git = F){
+create_deck <- function(deckdir){
   message('Creating slide directory at ', deckdir, '...')
   if (file.exists(deckdir)){
   	return('Directory already exists. Please choose a different name.')
   }
   scaffold = system.file('scaffold', package = 'slidify')
   system(sprintf('cp -r %s %s', scaffold, deckdir))
-  if (git == T){
-    init_repo()
-  }
   message('Finished creating slide directory...')
   message('Switching to slide directory...')
   setwd(deckdir)
+  if (Sys.which('git') != ""){
+  	init_repo()
+  }
   message('Opening slide deck...')
   file.edit('index.Rmd')
 }
 
 #' Initialize a git repository, create and switch to gh-pages branch.
 init_repo <- function(){
+	message('Initializing Git Repo')
 	system("git init")
 	system("git commit --allow-empty -m 'Initial Commit'")
 	message("Checking out gh-pages branch...")
