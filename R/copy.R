57a61fcd18d4a5688c077f73b6fb8955c9c51c5e
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2012-10-29 22:19:10 -0400

    update package reference to slidify

--- R/copy.R
+++ R/copy.R
@@ -10,16 +10,16 @@ copy_dir <- function(from, to){
 #' Copy libraries: framework, highlighter and widgets
 copy_libraries <- function(framework, highlighter, widgets){
 	copy_dir(
-		from = system.file('libraries', 'frameworks', framework, package = 'slidify2'),
+		from = system.file('libraries', 'frameworks', framework, package = 'slidify'),
 		to = file.path('libraries', 'frameworks', framework)
 	)
 	copy_dir(
-		from = system.file('libraries', 'highlighters', highlighter, package = 'slidify2'),
+		from = system.file('libraries', 'highlighters', highlighter, package = 'slidify'),
 		to = file.path('libraries', 'highlighters', highlighter)
 	)
 	for (widget in widgets){
 		copy_dir(
-			from = system.file('libraries', 'widgets', widget, package = 'slidify2'),
+			from = system.file('libraries', 'widgets', widget, package = 'slidify'),
 			to = file.path('libraries', 'widgets', widget)
 	)}	
 }
