86f12f3e178eb4307646736ea10cd0434e1c8d62
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2012-11-03 14:51:51 -0400

    add package.R

---
+++ R/package.R
@@ -0,0 +1,22 @@
+#' Create stunning presentations from R Markdown
+#' 
+#' Slidify is an R package to create, customize and publish HTML5 slide decks
+#' from R Markdown. 
+#' This is an alternative tool to Sweave with more flexible design and new
+#' features like cache and fine control of graphics. It is not limited to LaTeX
+#' and is ready to be customized to process other file formats. See the package
+#' website in the references for more information and examples.
+#' @docType package
+#' @name slidify-package
+#' @aliases slidify
+#' @author Ramnath Vaidyanathan <\url{http://www.slidify.org}>
+#' @seealso The core function in this package: \code{\link{slidify}}
+#' @references Full documentation and demos: \url{http://www.slidify.org};
+#'   FAQ's: \url{https://github.com/ramnathv/slidify/blob/master/FAQ.md}
+#' @import stringr
+#' @import knitr
+#' @import markdown
+#' @import stringr
+#' @import yaml
+#' @import whisker
+NULL
