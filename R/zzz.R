703e1e4932fe2170d3ef143e91a6e3c2cfcaba9c
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2012-11-06 13:11:38 -0500

    remove require(slidify) from .onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,6 @@
 .onLoad <- function(libname, pkgname){
 	options(rstudio.markdownToHTML = 
 		function(inputFile, outputFile) {      
-		  require(slidify)
 			slidify(inputFile, outputFile, knit_deck = FALSE)
 	})
 }
