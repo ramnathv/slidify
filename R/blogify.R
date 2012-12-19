4adb11cb936f8717bada3ecab8b549dc0f3ccdb8
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2012-12-18 22:28:51 -0500

    refactor render_* functions for blogify

--- R/blogify.R
+++ R/blogify.R
@@ -1,3 +1,53 @@
+#' Render a slide
+render_slide <- function(slide, layouts, payload){
+  tpl <- slide$tpl %||% 'slide'
+  payload = modifyList(payload, list(slide = slide))
+  slide$rendered = whisker.render(layouts[[tpl]], payload) %|% update_classes
+  slide$rendered = whisker.render(slide$rendered, payload)
+  return(slide)
+}
+
+#' Render slides
+render_slides <- function(slides, layouts, payload){
+  lapply(slides, render_slide, layouts = layouts, payload = payload)
+}
+
+
+#' Render page
+render_page <- function(page, payload){
+  if (page$mode == 'selfcontained'){
+    page$url[['lib']] <- page$url[['lib']] %||% 'libraries'
+    with(page, copy_libraries(framework, highlighter, widgets, url$lib))
+  }
+  
+  # add layouts, urls and stylesheets from frameworks, widgets and assets
+  page = page %|% add_urls %|% add_stylesheets %|% add_config_fr
+  layouts = get_layouts(page$url$layouts)
+  layouts = modifyList(layouts, list(javascripts = get_javascripts(page)))
+  
+  payload = modifyList(payload, list(page = page))
+  
+  # HACK ALERT. REALLY REALLY UGLY CODE TO FIX -----
+  page$slides = render_slides(page$slides, layouts, payload)
+  payload$page$slides = page$slides
+  
+  paste_all = function(...) paste(..., collapse = "\n")
+  page$content = do.call(paste_all, lapply(page$slides, pluck('rendered')))
+  payload$page$content = page$content
+  # HACK ALERT. REALLY REALLY UGLY CODE TO FIX -----
+  
+  outputFile = gsub("*.[R]?md$", '.html', page$file)
+  main = page$layout %||% 'deck'
+  cat(whisker.render(layouts[[main]], payload, partials = layouts), file = outputFile)
+}
+
+#' Render pages
+render_pages <- function(site, pages){
+  payload = list(site = site, pages = pages)
+  invisible(lapply(pages, render_page, payload = payload))
+}
+
+#' Render post
 render_post <- function(post){
   if (post$mode == 'selfcontained'){
     post$url[['lib']] <- post$url[['lib']] %||% 'libraries'
