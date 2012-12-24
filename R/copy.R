6d48311bb0ee17e59d537e6c4689ba39217a33fb
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2012-12-24 12:41:41 -0500

    refactor copy_libraries

--- R/copy.R
+++ R/copy.R
@@ -15,17 +15,13 @@ copy_dir <- function(from, to){
 #' @keywords internal
 #' @noRd
 copy_libraries <- function(framework, highlighter, widgets, url_lib, pkg = 'slidifyLibraries'){
-  copy_dir(
-    from = system.file('libraries', 'frameworks', framework, package = pkg),
-    to = file.path(url_lib, 'frameworks', framework)
-  )
-  copy_dir(
-    from = system.file('libraries', 'highlighters', highlighter, package = pkg),
-    to = file.path(url_lib, 'highlighters', highlighter)
-  )
-  for (widget in widgets){
+  copy_resource <- function(subdir, resource){
     copy_dir(
-      from = system.file('libraries', 'widgets', widget, package = pkg),
-      to = file.path(url_lib, 'widgets', widget)
-  )}  
+     from = system.file('libraries', subdir, resource, package = pkg),
+     to = file.path(url_lib, subdir, resource)
+    )
+  }
+  copy_resource('frameworks', framework)
+  copy_resource('highlighters', highlighter)
+  invisible(lapply(widgets, copy_resource, subdir = 'widgets'))
 }
