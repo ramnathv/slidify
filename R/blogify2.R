8bad37c6b9eee3196298d725115330a8274b967d
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-01-14 16:03:46 -0500

    add blogify functions to package

---
+++ R/blogify2.R
@@ -0,0 +1,59 @@
+pagify <- function(postFile){
+  if (file.exists('site.yml')){
+    site = yaml.load_file('site.yml')
+  } else {
+    site = list(x = 10)
+  }
+  page = parse_post(postFile)
+  in_dir(dirname(postFile), {
+    render_page(page, payload = list(site = site))
+  })
+}
+
+blogify <- function(blogDir = "."){
+  site = yaml.load_file('site.yml')
+  cwd   = getwd(); on.exit(setwd(cwd))
+  setwd(blogDir)
+  rmdFiles = dir(".", recursive = TRUE, pattern = '*.Rmd')
+  pages = lapply(rmdFiles, parse_post)
+  tags = get_pages_by_groups(pages, 'tags')
+  payload = list(site = site, pages = pages, tags = tags)
+  invisible(lapply(pages, function(page){
+    in_dir(dirname(page$file), 
+      render_page(page = page, payload = payload))
+  }))
+  message('Blogification Successful :-)')
+}
+
+parse_post <- function(postFile){
+  in_dir(dirname(postFile), {
+    inputFile = basename(postFile)
+    opts_chunk$set(fig.path = "assets/fig/", cache.path = '.cache/', cache = TRUE)
+    outputFile <- gsub(".Rmd", ".md", inputFile)
+    post = knit(inputFile, outputFile) %|% parse_deck
+    post$file = postFile
+    post$filename = tools:::file_path_sans_ext(inputFile)
+    if (!is.null(post$date)) {
+      post$date = as.Date(post$date, '%Y-%m-%d')
+    }
+    post$link = gsub("*.Rmd", ".html", post$file)
+    post$raw = read_file(inputFile)
+  })
+  return(post)
+}
+
+get_pages_by_groups <- function(pages, gby = 'tags'){
+  get_pages_by_group = function(g){
+    p = pages[sapply(pages, function(page) g %in% page[[gby]])]
+    p = lapply(p, '[', c('title', 'file', 'date', 'link'))
+    list(pages = p, name = g, count = length(p))
+  }
+  get_all_groups <- function(){
+    g = lapply(pages, '[[', gby)
+    Reduce('union', g)
+  }
+  x  = lapply(get_all_groups(), get_pages_by_group)
+  y = setNames(x, lapply(x, pluck('name')))
+  y$all = x
+  y
+}
